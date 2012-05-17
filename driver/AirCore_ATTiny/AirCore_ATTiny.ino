b14d9eb04b21b9f645ae2e117b02864ef647dbfd
Eero af Heurlin <eero.afheurlin@iki.fi>
2012-05-17 23:46:08 +0300

    added link to source of the TinyWireS -library

--- driver/AirCore_ATTiny/AirCore_ATTiny.ino
+++ driver/AirCore_ATTiny/AirCore_ATTiny.ino
@@ -1,5 +1,6 @@
 //TinyWire uses variable named I2C_SLAVE_ADDR, so we use the longer writing form here
 #define I2C_SLAVE_ADDRESS 0x4 // the 7-bit address (remember to change this)
+// Get this from http://www.arduino.cc/playground/Code/USIi2c
 #include <TinyWireS.h>
 
 byte angle = 128;
