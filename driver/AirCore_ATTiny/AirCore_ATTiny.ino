678a80508a26c20c8581aa947dad8da0c6595939
Eero af Heurlin <eero.afheurlin@iki.fi>
2012-05-17 22:43:49 +0300

    I2C setting of the HW-PWM works, but the lib is somehow funky it acknowledges every address after the slave address...

--- driver/AirCore_ATTiny/AirCore_ATTiny.ino
+++ driver/AirCore_ATTiny/AirCore_ATTiny.ino
@@ -2,13 +2,24 @@
 #define I2C_SLAVE_ADDRESS 0x4 // the 7-bit address (remember to change this)
 #include <TinyWireS.h>
 
-byte angle;
+byte angle = 128;
 
 void setup()
 {
-    TinyWireS.begin(I2C_SLAVE_ADDRESS);
-    
+    // Note that pin number are as per http://hlt.media.mit.edu/?p=1229
     pinMode(1, OUTPUT); // The HW-PWM -pin supported by the tiny core
+    pinMode(A3, OUTPUT); // OC1B-
+    pinMode(A2, OUTPUT); // OC1B
+
+    // Enable I2C Slave
+    TinyWireS.begin(I2C_SLAVE_ADDRESS);
+    /**
+     * Enabling internal pull-ups this way does not work    
+    digitalWrite(0, HIGH);
+    digitalWrite(2, HIGH);
+     */
+
+    digitalWrite(A3, HIGH);
 }
 
 void loop()
@@ -21,5 +32,8 @@ void loop()
     
     // Dummy for now just to test the I2C receiver
     analogWrite(1, angle);
+    // Pulse the pin to see activity
+    digitalWrite(A2, !digitalRead(A2));
+    digitalWrite(A3, !digitalRead(A3));
 }
 
