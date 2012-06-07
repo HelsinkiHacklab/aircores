b2c1aae783742f7be02fa97bb29e29b3a8ec7b24
Eero af Heurlin <eero.afheurlin@iki.fi>
2012-06-07 23:12:11 +0300

    Suovulas notes about pins and timers

--- driver/AirCore_ATTiny/AirCore_ATTiny.ino
+++ driver/AirCore_ATTiny/AirCore_ATTiny.ino
@@ -5,6 +5,24 @@
 
 byte angle = 128;
 
+/**
+ * Pin notes by Suovula
+ *
+ // I2C
+ arduino pin 0 = not(OC1A) = PORTB <- _BV(0) = SOIC pin
+5 (I2C SDA)
+ arduino pin 2 = = PORTB <- _BV(2) = SOIC pin 7 (I2C SCL)
+
+
+// Timer1 -> PWM
+ arduino pin 1 = OC1A = PORTB <- _BV(1) = SOIC pin 6
+
+arduino pin 3 = not(OC1B) = PORTB <- _BV(3) = SOIC pin 2
+ arduino pin 4
+= OC1B = PORTB <- _BV(4) = SOIC pin 3
+ */
+
+ 
 void setup()
 {
     // Note that pin number are as per http://hlt.media.mit.edu/?p=1229
